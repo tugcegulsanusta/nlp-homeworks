f06954ee9644b5c5eb1a08e1daa323bb3b701e58
TugceUsta <tugce.gulsan@gmail.com>
2023-11-05 14:08:03 +0300

    unigram- count and prob done

--- nlp-01/nlp-firsthomework.py
+++ nlp-01/nlp-firsthomework.py
@@ -1,19 +1,17 @@
 import re
+import codecs
 
-unigram_counts = {}
+
 unigram_prob = {}
 bigram_counts = {}
-tokenization_list = []
+bigram_prob = {}
 
 def read_data(file_name):
     data = []
-    f=open(file_name, mode="r")
+    f=codecs.open(file_name, mode="r",encoding="utf-8")
     data = f.read()
-    count = 0
-    for tokens in data:
-        if tokens.endswith(".") or tokens.endswith("!") or tokens.endswith("?"):
-            count += 1
-    #print(f"number of sentences: {count}")
+    f.close()
+
     myRegex_start = "(?:^|(?:[\*]\s+))"
     myRegex_end = "[.,!?]"
     add_end = re.sub(myRegex_end, " </s>*", data)
@@ -22,29 +20,31 @@ def read_data(file_name):
     tokenization_list[-1] = "</s>"
     return  tokenization_list
 
-def unigram_calculation(file_name):
-    tokenization_list = tokenization(file_name)
+def total_counter(tokenization_list):
     total_count = 0
     for word in tokenization_list:
         total_count+=1
+    return total_count
+
+def unigram_counter(tokenization_list):
+    unigram_counts = {}
+    for word in tokenization_list:
         if word not in unigram_counts:
             unigram_counts[word] = 1
         else:
             unigram_counts[word] += 1
+    return unigram_counts
 
-    print("Unigram kelime sayıları: ")
-    print("unigram listesi: " , unigram_counts)
-    
-    #for word in unigram_counts:
-     #   print(word,unigram_counts[word])
-    print("Unigram olasılıkları: ")
+def unigram_prob_counter(tokenization_list, unigram_counts):
+    total_count = total_counter(tokenization_list)
+    unigram_strings = []
     for word in unigram_counts:
-        unigram_prob = {word,unigram_counts[word]/total_count}
-    return unigram_prob
+        unigram_probabilities = {word,unigram_counts[word]/total_count}
+        unigram_strings.append(unigram_probabilities)
+    return unigram_strings
 
-def bigram_calculations(file_name):
-    tokenization_list = read_data(file_name)
-    
+
+def bigram_counter(tokenization_list):
     for i in range(len(tokenization_list)-1):
         temp = (tokenization_list[i], tokenization_list[i+1])
         if not temp in bigram_counts:
@@ -55,13 +55,26 @@ def bigram_calculations(file_name):
 
     return bigram_counts
 
-#bigram_calculations("deneme.txt", bigram_counts)
-#unigram_calculation("hw01_tinytr.txt")
-#unigram_calculation("hw01_bilgisayar.txt")
+def write_file(file_name_write, tokenization_list,  unigram_counts):
+    #fw = codecs.open(file_name_write,mode="w",encoding="utf-8")
+    unique_word_counts_str = str(len(unigram_counts))
+    unigram_counts_str =str(unigram_counts)
+    unigram_probability_str = str(unigram_prob_counter(tokenization_list, unigram_counts))
+    with codecs.open(file_name_write, 'w', encoding="utf-8") as fw:
+        fw.write("Unique kelime sayısı : ")
+        fw.write(unique_word_counts_str)
+        fw.write("\nUnigram sayıları: \n")
+        fw.write(unigram_counts_str)
+        fw.write("\nUnigram olasılıkları: \n")
+        fw.write(unigram_probability_str)
 
-
+    #bigram_data = str(bigram_calculations(file_name_open))
+    fw.close()
 
 if __name__ == "__main__":
-    file_name = "hw01_tinytr.txt"
+    file_name = "deneme.txt"
     tokenization_list = read_data(file_name)
-    bigram_calculations(file_name)
+    unigram_counts = unigram_counter(tokenization_list)
+    #bigram_calculations(file_name)
+    #unigram_calculation(file_name)
+    write_file("sonuc.txt", tokenization_list,  unigram_counts)
