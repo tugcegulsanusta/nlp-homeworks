6746cbfe7ac2d788a13417f4ac3e139b9e500152
TugceUsta <tugce.gulsan@gmail.com>
2023-11-06 14:14:18 +0300

    bigram- count and prob done

--- nlp-01/nlp-firsthomework.py
+++ nlp-01/nlp-firsthomework.py
@@ -1,12 +1,10 @@
 import re
 import codecs
 
-
 unigram_prob = {}
 bigram_counts = {}
-bigram_prob = {}
 
-def read_data(file_name):
+def tokenizeFile(file_name):
     data = []
     f=codecs.open(file_name, mode="r",encoding="utf-8")
     data = f.read()
@@ -20,61 +18,71 @@ def read_data(file_name):
     tokenization_list[-1] = "</s>"
     return  tokenization_list
 
-def total_counter(tokenization_list):
-    total_count = 0
-    for word in tokenization_list:
-        total_count+=1
-    return total_count
-
-def unigram_counter(tokenization_list):
-    unigram_counts = {}
-    for word in tokenization_list:
-        if word not in unigram_counts:
-            unigram_counts[word] = 1
+def countUniqueItem( list ):
+    uniqueCounts = {}
+    for item in list:
+        if item not in uniqueCounts:
+            uniqueCounts[item] = 1
         else:
-            unigram_counts[word] += 1
-    return unigram_counts
+            uniqueCounts[item] += 1
+    return uniqueCounts
 
-def unigram_prob_counter(tokenization_list, unigram_counts):
-    total_count = total_counter(tokenization_list)
+def tokenListToBigram(tokenization_list):
+    bigram_list = []
+    for i in range (len(tokenization_list)-1):
+        temp = (tokenization_list[i], tokenization_list[i+1])
+        bigram_list.append(temp)
+    return bigram_list
+
+def findUnigramProb(tokenization_list, unigram_counts):
+    total_count = len(tokenization_list)
     unigram_strings = []
     for word in unigram_counts:
         unigram_probabilities = {word,unigram_counts[word]/total_count}
         unigram_strings.append(unigram_probabilities)
     return unigram_strings
 
+def findBigramProb(bigram_list, unigram_counts, bigram_counts):
+    listOfProb = {}
+    for bigram in bigram_list:
+        word1 = bigram[0]
+        word2 = bigram[1]
+        listOfProb[bigram] = (bigram_counts[bigram])/ (unigram_counts[word1])
 
-def bigram_counter(tokenization_list):
-    for i in range(len(tokenization_list)-1):
-        temp = (tokenization_list[i], tokenization_list[i+1])
-        if not temp in bigram_counts:
-            bigram_counts[temp] = 1
-        else:
-            bigram_counts[temp] += 1
-    print("Bigram listesi: ", bigram_counts  )
+    return listOfProb
 
-    return bigram_counts
-
-def write_file(file_name_write, tokenization_list,  unigram_counts):
-    #fw = codecs.open(file_name_write,mode="w",encoding="utf-8")
-    unique_word_counts_str = str(len(unigram_counts))
-    unigram_counts_str =str(unigram_counts)
-    unigram_probability_str = str(unigram_prob_counter(tokenization_list, unigram_counts))
+def write_file(file_name_write,  unigram_counts, unigram_probability, bigram_counts, bigram_probability, sentenceCount, totalWordCount):
+    
     with codecs.open(file_name_write, 'w', encoding="utf-8") as fw:
-        fw.write("Unique kelime sayısı : ")
-        fw.write(unique_word_counts_str)
+
+        fw.write("Cümle sayısı : ")
+        fw.write(str(sentenceCount))
+        fw.write("\nToplam kelime sayısı : ")
+        fw.write(str(totalWordCount))
+        fw.write("\nUnique kelime sayısı : ")
+        fw.write(str(len(unigram_counts)))
         fw.write("\nUnigram sayıları: \n")
-        fw.write(unigram_counts_str)
+        fw.write(str(unigram_counts))
         fw.write("\nUnigram olasılıkları: \n")
-        fw.write(unigram_probability_str)
+        fw.write(str(unigram_probability))
+        fw.write("\nBigram sayıları: \n")
+        fw.write(str(bigram_counts))
+        fw.write("\nBigram olasılıkları : \n")
+        fw.write(str(bigram_probability))
 
     #bigram_data = str(bigram_calculations(file_name_open))
     fw.close()
 
 if __name__ == "__main__":
-    file_name = "deneme.txt"
-    tokenization_list = read_data(file_name)
-    unigram_counts = unigram_counter(tokenization_list)
-    #bigram_calculations(file_name)
-    #unigram_calculation(file_name)
-    write_file("sonuc.txt", tokenization_list,  unigram_counts)
+    fileName = "deneme.txt"
+    tokenizationList = tokenizeFile(fileName)
+    unigramCounts = countUniqueItem(tokenizationList)
+    unigram_probability= findUnigramProb(tokenizationList, unigramCounts)
+    bigram_list = tokenListToBigram(tokenizationList)
+    bigram_counts = countUniqueItem(bigram_list)
+    bigram_probability = findBigramProb(bigram_list,unigramCounts,bigram_counts)
+    sentenceCount = unigramCounts["</s>"]
+    totalWordCount = len(tokenizationList)
+
+    
+    write_file("sonuc.txt",unigramCounts, unigram_probability, bigram_counts, bigram_probability, sentenceCount, totalWordCount)
